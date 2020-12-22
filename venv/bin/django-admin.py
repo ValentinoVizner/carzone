cdefece538901d904f79dbd52132b43689cbee21
Valentino Vizner <valentino.vizner@axilis.com>
2020-12-22 10:43:20 +0100

    my first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/valentinovizner/Documents/axilis_dev/1_Python_Projects/Build_Python_Django_Project_course/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
